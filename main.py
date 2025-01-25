436b66fd30ec01cadf19b5f7cf2966d095db060e
vudkas <133009054+vudkas@users.noreply.github.com>
2025-01-25 09:08:05 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import telebot
+from flask import Flask, request
+import os
+
+token = "6928284331:AAF7BI7UJqkfN7BZ2lIsIbwRpuh1gsE_cbI"
+bot = telebot.TeleBot(token)
+
+app = Flask(__name__)
+
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return '', 200
+
+def set_webhook():
+    url = "https://YOUR_URL/webhook"
+    bot.remove_webhook()
+    bot.set_webhook(url=url)
+
+if __name__ == "__main__":
+    set_webhook()
+    app.run(host="0.0.0.0", port=5000)
